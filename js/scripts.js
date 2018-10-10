cd48a14fb1763d47a0f12b0647b5a05ac58528a5
polyedre <lucas_henry@live.fr>
2018-10-10 16:00:59 +0200

    Transformation du serveur Node.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,43 @@
+var app = require('express')();
+var http = require('http');
+var https = require('https');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 
-var p_roues = document.getElementById("roues");
-var p_tension = document.getElementById("tension");
+app.set('view engine', 'pug');
 
-var json;
-var xobj = new XMLHttpRequest();
-xobj.overrideMimeType("application/json");
-xobj.open('GET', 'test.json', true); // Replace 'my_data' with the path to your file
-xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-        json = xobj.responseText;
-    }
-};
-var vrai_json = JSON.parse(json);
+app.listen(8080);
+
+console.log("Server started at http://localhost:"+ 8080);
+
+app.get('/', (req_app, res_app) => {
+
+    console.log("Entrée GET");
+
+    http.get('http://127.0.0.1:3000', (resp) => {
+
+        let data = '';
+
+        resp.on("data", (chunk) => {
+            data += chunk;
+        });
+
+        resp.on("end", () => {
+            console.log("Tout le fichier a été récupéré.");
+            json = JSON.parse(data);
+
+            app.render('affichage', json, (err, html) => {
+                if (err) throw err;
+                res_app.send(html);
+            });
+
+        });
+
+        resp.on("error", (err) => {
+            console.log("Error : " + err.message);
+        });
+
+    });
+
+});
