760052701dd890d41ea664a8ad66fced3c4a36d6
polyedre <lucas_henry@live.fr>
2018-10-10 16:06:24 +0200

    Réorganisation des dossiers du projet

---
+++ Serveur_python/run_server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3.6
+
+from http import server
+
+def run(server_class=server.HTTPServer, handler_class=server.BaseHTTPRequestHandler, port=3000):
+    """Lance le server."""
+    print("Lancement du server sur le port {}.".format(port))
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+class MyHTTPRequestHandler(server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        self.send_response(200)
+
+        self.send_header('content-type', 'text-json')
+        self.end_headers()
+
+        self.wfile.write( b"""{
+        \"tours_1\" : 60,
+        \"tours_2\" : 70
+        }""")
+
+        return
+
+if __name__ == '__main__':
+    run(handler_class=MyHTTPRequestHandler)
