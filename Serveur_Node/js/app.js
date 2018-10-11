00c1d8c7fdba028148658e8ea38a18ecbc79e57d
polyedre <lucas_henry@live.fr>
2018-10-11 19:40:45 +0200

    Remplacement de Node.js par Vue.js sans serveur

--- Serveur_Node/js/app.js
+++ Serveur_Node/js/app.js
@@ -2,8 +2,8 @@ new Vue({
     el : '#app',
     data : {
         robot_name : "Nom du Robot",
-        roue_1 : 30,
-        roue_2 : 30,
+        roue_1 : 00,
+        roue_2 : 00,
         tension : 12,
         intensite : 0.5,
         vitesse : 2,
@@ -11,11 +11,27 @@ new Vue({
     },
     methods : {
         ask_data: function () {
-            const http = new XMLHttpRequest();
-            const url = 'http://localhost:8080/data.json';
-            http.open("GET", url);
-            http.send();
 
+            setInterval( () => {
+                const http = new XMLHttpRequest();
+                const url = 'http://localhost:3000';
+                http.open("GET", url, true);
+                http.send();
+
+                http.onreadystatechange = () => {
+                    if (http.readyState === 4 && http.status === 200) {
+                        json_data = JSON.parse(http.responseText);
+                        console.log(json_data);
+                        this.robot_name = json_data.robot_name;
+                        this.roue_1 = json_data.roue_1;
+                        this.roue_2 = json_data.roue_2;
+                        this.tension = json_data.tension;
+                        this.intensite = json_data.intensite;
+                        this.vitesse = json_data.vitesse;
+                        this.style_couleur = json_data.style_couleur;
+                    }
+                };
+                }, 1000);
         }
     }
 });
--- Serveur_Node/scripts.js
+++ Serveur_Node/scripts.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http');
 var https = require('https');
 var bodyParser = require('body-parser');
+var fs = require("fs");
 
 app.use(bodyParser.json());
 
@@ -11,9 +12,36 @@ app.listen(8080);
 
 console.log("Server started at http://localhost:"+ 8080);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req_app, res_app) => {
 
     console.log("Entrée GET");
+
+
+    var options = {
+        root : ".",
+        headers: {
+            'content-type': 'text/html',
+        }
+    };
+
+    res_app.sendStatus();
+    res_app.sendFile("views/affichage.html", options);
+
+    res_app.end();
+
+});
+
+app.get('/data.json', (req_app, res_app) => {
+
+    console.log("Requête entrante : demande du JSON");
+
+    res_app.set
 
     http.get('http://127.0.0.1:3000', (resp) => {
 
@@ -27,10 +55,7 @@ app.get('/', (req_app, res_app) => {
             console.log("Tout le fichier a été récupéré.");
             json = JSON.parse(data);
 
-            app.render('affichage', json, (err, html) => {
-                if (err) throw err;
-                res_app.send(html);
-            });
+            res_app.sendFile("/views/affichage.html", {root : "."});
 
         });
 
